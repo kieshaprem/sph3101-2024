0b18e7ba666925243f98d07b8495bc880481fba7
Kiesha Prem <12025079+kieshaprem@users.noreply.github.com>
2024-08-19 07:06:57 +0800

    updates to practical 1

---
+++ practical/scripts/4_linear-regression_practical.r
@@ -0,0 +1,39 @@
+############# In-class practical ############# 
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+## Plot the relationship between birth weight and gestational weeks
+# which do you think is the independent and dependent variable? 
+
+
+## Describe the relationship
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+##  Use linear regression to investigate the relationship between birth weight 
+##  and gestational weeks.  
+
+
+
+## Obtain the equation for the regression of birthweight on gestational weeks. 
+
+
+
+## Now you can test the hypothesis that slope = 0. 
+
+
+
+## Describe the relationship
+
+
+
+
+# Add the fiited regression line to the graph  
+
+
+
+
+
+
