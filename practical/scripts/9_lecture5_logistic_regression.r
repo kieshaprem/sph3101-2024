9863c2baa1802959b7b3dc4e6c4da65e476b3b29
Kiesha Prem <12025079+kieshaprem@users.noreply.github.com>
2024-09-09 09:24:14 +0800

    Create 9_lecture5_logistic_regression.r

---
+++ practical/scripts/9_lecture5_logistic_regression.r
@@ -0,0 +1,13 @@
+
+############# In-class practical ############# 
+
+load('data/sepsis.ungrouped.RData')
+
+
+# Task: plot the relationship between mortality and APACHE II score:
+
+
+# Task: Can you write the linear predictor of the simple logistic regression model?
+# To perform a simple logistic regression with APACHE II score as a predictor of mortality, use the glm function 
+
+
